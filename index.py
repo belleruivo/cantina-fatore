0090ab90e05f54562c772b7e4e75e973ef310633
Isabelle Ruivo dos Santos <isabelleruivosantos@gmail.com>
2024-11-12 11:55:43 -0300

    alguns comentários para o restante do grupo

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from app import create_app
+# da pasta app ele importou o create_app que tá no __init__.py
 
-app = create_app()
+app = create_app() # instancia a aplicação
 
 if __name__ == "__main__":
+    # roda a aplicação no localhost na porta 5000, com o debug ativado porém não rolou precisa rodar o flask debug (modo desenvolvimento)
     app.run(debug=True)
