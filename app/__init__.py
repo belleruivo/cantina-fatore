da62808e9c5e2924dc86c72ca734d1a9a2664f0e
Isabelle Ruivo dos Santos <isabelleruivosantos@gmail.com>
2024-11-12 11:55:53 -0300

    __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from app.controllers.main_controller import home
 
+# inicializa os pacotes
+
 def create_app():
     app = Flask(__name__)
     app.secret_key = "9wefjfsdfsdaodaofjejcaqwqwewqeoooopwq"
